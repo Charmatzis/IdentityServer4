ee95db169ee306adbf243c08f521865ee4fe691d
Christos <xr_xarmatzis@outlook.com>
2016-02-26 21:55:59 +0200

    Update Messages.cs

--- src/IdentityServer4/Resources/Messages.cs
+++ src/IdentityServer4/Resources/Messages.cs
@@ -12,23 +12,23 @@ namespace IdentityServer4.Core.Resources
             return typeof(Messages).GetField(name)?.GetValue(null)?.ToString();
         }
 
-        public const string ClientIdRequired = "Client identifier is required";
-        public const string ExternalProviderError = "There was an error logging into the external provider.The error message is: {0}";
-        public const string invalid_request = "The client application made an invalid request.";
-        public const string invalid_scope = "The client application tried to access a resource it does not have access to.";
-        public const string InvalidUsernameOrPassword = "Invalid username or password";
-        public const string MissingClientId = "client_id is missing";
-        public const string MissingToken = "Token is missing";
-        public const string NoExternalProvider = "The external login provider was not provided.";
-        public const string NoMatchingExternalAccount = "Invalid Account";
+        public const string ClientIdRequired = "Η ταυτοποίηση του πελάτη είναι απαραίτητη";
+        public const string ExternalProviderError = "Υπήρξε σφάλμα κατά την είσοδο. Το σφάλμα είναι {0}";
+        public const string invalid_request = "Η εφαρμογή του πελάτη δημιούργησε έναν άκυο αίτημα.";
+        public const string invalid_scope = "Η εφαρμογή του πελάτη επιχείρησε να εισέλθει χωρίς να έχει την κατάλληλη έγκριση.";
+        public const string InvalidUsernameOrPassword = "Άκυρο όνομα χρήστη ή κωδικός";
+        public const string MissingClientId = "Λέιπει η ταυτότητα του πελάτη";
+        public const string MissingToken = "Token λείπει";
+        public const string NoExternalProvider = "Η εξωτερική είσοδος του παρόχου δεν ήταν ικανός να παράξει άδεια.";
+        public const string NoMatchingExternalAccount = "Άκυρος λογαριασμός";
         public const string NoSignInCookie = "There is an error determining which application you are signing into.Return to the application and try again.";
         public const string NoSubjectFromExternalProvider = "Error authenticating with external provider";
-        public const string PasswordRequired = "Password is required";
-        public const string SslRequired = "SSL is required";
+        public const string PasswordRequired = "Ο κωδικός είναι απαραίτητος";
+        public const string SslRequired = "SSL είναι απαραίτητο";
         public const string unauthorized_client = "The client application is not known or is not authorized.";
-        public const string UnexpectedError = "There was an unexpected error";
+        public const string UnexpectedError = "Υπήρξε ένα άγνωστο σφάλμα";
         public const string unsupported_response_type = "The authorization server does not support the requested response type.";
         public const string UnsupportedMediaType = "Unsupported Media Type";
-        public const string UsernameRequired = "Username is required";
+        public const string UsernameRequired = "Το όνομα χρήστη είναι απαραίτητο";
     }
 }
